a239d67b202927edb6c0746d7caabd50b1613265
GLORIFIEDINTEL <glorifiedinteligence@outlook.com>
2025-06-12 18:39:15 +0100

    commit
    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,7 +8,7 @@ const LandingPage = () => {
         <h2 className="ranking">#Our Passion</h2>
         <h3 className="main-text">Your Journey to a Meaningful Career in Nursing and Midwifery Starts Here</h3>
         <p className="sub-text">
-          (U.S. News & World Report, 2023)
+          (Shajid College of Nursing and Midwifery) is a premier institution dedicated to nurturing the next generation of healthcare professionals. Our mission is to provide exceptional education and training in nursing and midwifery, empowering students to excel in their careers and make a positive impact on the health and well-being of individuals and communities.
         </p>
       </div>
     </div>
