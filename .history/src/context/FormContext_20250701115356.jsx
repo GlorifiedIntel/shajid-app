067a1e6271b596b0f7a25297ed69d020b7db56fa
kcminchakpu <glorifiedinteligence@outlook.com>
2025-07-01 11:54:54 +0100

    commit
    update

---
+++ .history/src/context/FormContext_20250701115356.jsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { createContext, useContext, useState } from 'react';
+
+const FormContext = createContext();
+
+export const FormProvider = ({ children }) => {
+  const [formData, setFormData] = useState({});
+
+  const updateFormData = (step, data) => {
+    setFormData((prev) => ({
+      ...prev,
+      [step]: data,
+    }));
+  };
+
+  return (
+    <FormContext.Provider value={{ formData, updateFormData }}>
+      {children}
+    </FormContext.Provider>
+  );
+};
+
+export const useFormContext = () => useContext(FormContext);
